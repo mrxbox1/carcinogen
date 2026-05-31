a05830660b62502982b10ff694a9753f46639f4b
mrxbox1 <gabriel.holub@outlook.com>
2026-05-31 20:21:19 +0200

    carcinogenous madness ensues

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 
-md_string = """# Pull and peel licorice
-## bees
-According to all known laws of aviation, there is no way a bee should be able to fly.
-### bees2
-Its wings are too fat to lift its fat little body off the ground.
-#### bees3
-The bee, of course, flies anyway.
-##### bees4
-Because bees don't care what humans think is possible.
-###### bees5
-Yellow, black, yellow, black, yellow, black, yellow, black...Ooh! Black and yellow! Let's shake it up a little!"""
+md_sample = open('sample.md', 'r').readlines()
 
-def md_to_html(string):
-    string = string.split("\n")
-    final_html = "<html><body>"
+def md_to_html(md_text):
+    final_html = "<!DOCTYPE html><html><body>"
 
-    for line in string:
+    for line in md_text:
         if line.startswith("#"):
 
             # gotta count all the hashes to determine the heading
@@ -29,10 +18,10 @@ def md_to_html(string):
         else:
             final_html += '<p>' + line + '</p>'
 
-    final_html += '</body></html>'
+    final_html += "</body></html>"
 
     return final_html
 
 if __name__ == "__main__":
     # test functionality
-    print(md_to_html(md_string))
+    print(md_to_html(md_sample))
---
+++ sample.md
@@ -0,0 +1,18 @@
+# This should be h1
+Hello, world!
+
+## This should be h2
+Getting smaller...
+
+### This should be h3
+...and smaller...
+
+#### This should be h4
+...and smaller, yet again...
+
+##### This should be h5
+...until the fabric of the universe starts breaking...
+
+###### This should be h6
+...and the world no longer looks the same.
+Cool.
