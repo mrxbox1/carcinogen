e2ff152701286a73343c8b08114e3f2902a05722
mrxbox1 <gabriel.holub@outlook.com>
2026-06-01 21:10:39 +0200

    Python....packaging..?

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='carcinogen',
+    version='0.1.1 (2026-06-01)',
+    description='MD-to-HTML "converter"',
+    author='mrxbox1',
+    packages=['carcinogen']
+)
